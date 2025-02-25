8ccc123362021d2e6c7ddaabc37b8456b8f16155
Shivani Shankar <shivanishankar101@gmail.com>
2025-02-25 12:46:06 -0500

    Initial commit for homework

---
+++ homework.py
@@ -0,0 +1,11 @@
+# Example: Sum of two numbers
+def sum(a, b):
+    return a+b
+
+# Problem 1: Product of two numbers
+
+# Problem 2: Power of two numbers
+
+# Problem 3: Kenobi greeting
+
+# Problem 4: sum of three numbers (write this without using the + symbol at all, i.e. by instead using the sum function defined above)
