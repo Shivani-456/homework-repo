4de15c3748f950eb06dc2bf96b7605244f78fd12
Shivani Shankar <shivanishankar101@gmail.com>
2025-02-25 12:51:48 -0500

    Initial commit for homework

--- homework.py
+++ homework.py
@@ -3,9 +3,14 @@ def sum(a, b):
     return a+b
 
 # Problem 1: Product of two numbers
-
+def product(a, b):
+    return a * b
 # Problem 2: Power of two numbers
-
+def power(base, exponent):
+    return base ** exponent
 # Problem 3: Kenobi greeting
-
+def kenobi(s):
+    print("Hello there,", s)
 # Problem 4: sum of three numbers (write this without using the + symbol at all, i.e. by instead using the sum function defined above)
+def doubleSum(a, b, c):
+    return sum([a, b, c])
